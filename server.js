a1c93a0988bb807e03bba9d91526132fd668dd36
Jovana Stevanovic <Jovana@Jovanas-MacBook-Pro.local>
2017-09-18 20:37:29 +0200

    Add client side React app and proxy to Node API

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const api = require('./routes/api');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
